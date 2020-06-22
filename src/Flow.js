9db298081b717e6563cd36629e2dda3dd26fce8e
Michell Ocana do Espirito Santo <michell.santo@cwi.com.br>
2020-06-22 17:31:05 -0300

    feat: flow chart slide

---
+++ src/Flow.js
@@ -0,0 +1,137 @@
+import React from 'react'
+import 'excalidraw/dist/excalidraw.min.css'
+
+import flow from './images/flow.svg'
+
+import './Flow.css'
+
+function Flow() {
+  return (
+    <div className='flow'>
+      <a
+        target='_blank'
+        rel='noferrer noopener'
+        href='https://excalidraw.com/#json=6035265537703936,Z0NmYPuf5-HBLsd6n68KbQ'
+      >
+        <svg className='icon' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 501.072 496.174'>
+          <path
+            fill='none'
+            stroke='#04353c'
+            d='M-.451 1.05c-.126 72.25.231 360.807.264 432.861M1.513.557c-.35 71.887-2.302 359.93-2.746 431.71'
+            transform='translate(82.192 9.443)'
+          ></path>
+          <path
+            fill='none'
+            stroke='#5c940d'
+            d='M-.7-.023C-51.319 50.229-252.552 251.719-303.12 302.209M1.131-1.081C-49.691 49.246-253.575 252.452-304.05 303.33'
+            transform='translate(386.633 58.42)'
+          ></path>
+          <path
+            fill='none'
+            stroke='#04353c'
+            d='M.756-.755C-67.279-.637-339.326.291-407.71.47m407.404.994c-68.174-.227-340.156-2.926-407.978-2.87'
+            transform='translate(490.316 443.697)'
+          ></path>
+          <path
+            fill='none'
+            stroke='#5c940d'
+            d='M.365 1.016C-52.545 53.978-263.95 265-317.17 317.834M-.903.504c-53.007 53.146-264.11 265.64-316.709 318.78'
+            transform='translate(471.615 121.65)'
+          ></path>
+          <text
+            y='45'
+            fill='#5c940d'
+            fontFamily='Virgil'
+            fontSize='39.101'
+            style={{ whiteSpace: 'pre' }}
+            transform='rotate(-45 421.366 -127.216)'
+          >
+            FLOW
+          </text>
+          <text
+            y='23'
+            fill='#04353c'
+            fontFamily='Virgil'
+            fontSize='20'
+            style={{ whiteSpace: 'pre' }}
+            transform='translate(209.462 454.174)'
+          >
+            HABILIDADES
+          </text>
+          <text
+            y='23'
+            fill='#04353c'
+            fontFamily='Virgil'
+            fontSize='20'
+            style={{ whiteSpace: 'pre' }}
+            transform='rotate(-90 156.972 119.78)'
+          >
+            DESAFIOS
+          </text>
+          <text
+            y='23'
+            fill='#04353c'
+            fontFamily='Virgil'
+            fontSize='20'
+            style={{ whiteSpace: 'pre' }}
+            transform='translate(113.77 58.02)'
+          >
+            ANSIEDADE
+          </text>
+          <text
+            y='23'
+            fill='#04353c'
+            fontFamily='Virgil'
+            fontSize='20'
+            style={{ whiteSpace: 'pre' }}
+            transform='translate(392.846 361.097)'
+          >
+            TÉDIO
+          </text>
+          <text
+            y='23'
+            fill='#364fc7'
+            fontFamily='Virgil'
+            fontSize='20'
+            style={{ whiteSpace: 'pre' }}
+            transform='translate(436.5 452.79)'
+          >
+            ALTA
+          </text>
+          <text
+            y='23'
+            fill='#c92a2a'
+            fontFamily='Virgil'
+            fontSize='20'
+            style={{ whiteSpace: 'pre' }}
+            transform='translate(10 405.866)'
+          >
+            BAIXO
+          </text>
+          <text
+            y='23'
+            fill='#c92a2a'
+            fontFamily='Virgil'
+            fontSize='20'
+            style={{ whiteSpace: 'pre' }}
+            transform='translate(82.538 452.79)'
+          >
+            BAIXA
+          </text>
+          <text
+            y='23'
+            fill='#364fc7'
+            fontFamily='Virgil'
+            fontSize='20'
+            style={{ whiteSpace: 'pre' }}
+            transform='translate(21.115 17.405)'
+          >
+            ALTO
+          </text>
+        </svg>
+      </a>
+    </div>
+  )
+}
+
+export default Flow
